9b9dcc46d1383b9d03917a82af443bf7ea2de1f2
Fatimah Novi Warkita <scoutnovie@gmail.com>
2013-10-26 20:25:37 +0700

    Praktikum Ke-4
    Praktikum Ke-4

---
+++ 110533430592/Praktikum Ke-4/5. Cookie dan Session/Latihan/3. Autentikasi Halaman/auth.php
@@ -0,0 +1,78 @@
+<?php
+defined('_VALID') or die('not allowed');
+
+function init_login()
+{
+	//Simulasi data account nama dan password
+	$nama = 'admin';
+	$pass = 'admin';
+	
+	if (isset($_POST['nama']) && isset($_POST['pass']))
+	{
+		$n = trim($_POST['nama']);
+		$p = trim($_POST['pass']);
+		
+		if( ($n === $nama) && ($p === $pass))
+		{
+			//jika sama, set cookie
+			setcookie('nlogin', $n);
+			setcookie('time', time());
+			
+			//redireksi
+			?>
+			<script type="text/javascript">
+			document.location.href="./";
+			</script>
+		
+		<?php
+		}
+		else
+		{
+			echo 'Nama/Password Tidak Sesuai';
+			return false;
+		}
+	}
+}
+
+function validate()
+{
+	if (!isset($_COOKIE['nlogin']) || !isset($_COOKIE['time']) )
+	{
+	?>
+	<div class="inner">
+	<form action="" method="post">
+	<table border =0 cellpadding=5>
+		<tr>
+			<td>Nama</td>
+			<td><input type="text" name="nama" /></td>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td><input type="password" name="pass" /></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" value="LOGIN" /></td>
+		</tr>
+	</table>
+	</form>
+	</div>
+	<?php
+	exit;
+}
+}
+
+	if(isset($_GET['m']) && $_GET['m'] == 'logout')
+	{
+		if(isset($_SESSION['nlogin']))
+		{
+			unset($_SESSION['nlogin']);
+		}
+	?>
+	
+	<script type="text/javascript">
+	document.location.href="./";
+	</script>
+<?php
+}
+?>
